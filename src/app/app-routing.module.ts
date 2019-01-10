0935a0ac71db74b550724e194cf626c2fb80fca2
Marcel Maier <marcel.maier@ssi-schaefer.com>
2019-01-10 10:56:36 +0100

    added routing for dashboard and transportUnits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { TransportUnitsComponent } from './components/transport-units/transport-units.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'transport-units',
+    component: TransportUnitsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
