4e6e144bbb9ce7c036eb4d32858e09a0ccf36d37
Marcel Maier <marcel.maier@ssi-schaefer.com>
2019-01-10 14:33:00 +0100

    added new servics and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 
 /* Import MaterialModules */
@@ -10,8 +9,14 @@ import { MaterialModules } from './material.modules';
 
 /* Import BrowserAnimations */
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/* Components */
+import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { TransportUnitsComponent } from './components/transport-units/transport-units.component';
+
+/* Template Components */
+import { TableComponent } from './components/templates/table/table.component';
 
 /* Imports for HTTP calls */
 import { HttpClientModule } from '@angular/common/http';
@@ -19,11 +24,15 @@ import { HttpClientModule } from '@angular/common/http';
 /* Services */
 import { TransportUnitService } from './services/transport-unit.service';
 
+/* Template Services */
+import { TableTemplateService } from './services/template-services/table.service';
+
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    TransportUnitsComponent
+    TransportUnitsComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
@@ -33,6 +42,7 @@ import { TransportUnitService } from './services/transport-unit.service';
     HttpClientModule
   ],
   providers: [
+    TableTemplateService,
     TransportUnitService
   ],
   bootstrap: [AppComponent]
