5d68150e444fc33cdedde91a411bfa05f51d0795
Marcel Maier <marcel.maier@ssi-schaefer.com>
2019-01-11 10:00:51 +0100

    added paginator template

---
+++ src/app/services/template-services/paginator.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class PaginatorTemplateService {
+
+  length: number;
+  pageSize: number;
+  pageSizes: number[];
+  index: number;
+
+  constructor() { }
+
+  getLength(): number {
+    return this.length;
+  }
+
+  getPageSize(): number {
+    return this.pageSize;
+  }
+
+  getPageSizes(): number[] {
+    return this.pageSizes;
+  }
+
+  setLength(length: number) {
+    this.length = length;
+  }
+
+  setPageSize(size: number) {
+    this.pageSize = size;
+  }
+
+  setPageSizes(sizes: number[]) {
+    this.pageSizes = sizes;
+  }
+
+  getIndex(): number {
+    return this.index;
+  }
+
+  setIndex(index: number): void {
+    this.index = index;
+  }
+}
